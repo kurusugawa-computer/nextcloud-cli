350c4cf7a21140940a6aa639888809aa27f16c6b
pfe171 <pureforeverearth@outlook.jp>
2024-09-11 12:57:04 +0900

    条件分岐を簡潔に修正

--- cmd/upload/upload.go
+++ cmd/upload/upload.go
@@ -336,10 +336,12 @@ func upload(ctx *ctx, src string, fi os.FileInfo, dst string, igs []ignPtn) {
 	case 2: // DeconflictOverwrite
 
 	case 3: // DeconflictNewest
-		if _, fis, err := getFileInfo(ctx, dst); err == nil && !fi.ModTime().After(fis[0].ModTime()) {
-			fmt.Println("skip older file: " + src)
-			return
-		} else if err != nil && !errors.Is(errors.Cause(err), fs.ErrNotExist) {
+		if _, fis, err := getFileInfo(ctx, dst); err == nil {
+			if !fi.ModTime().After(fis[0].ModTime()) {
+				fmt.Println("skip older file: " + src)
+				return
+			}
+		} else if !errors.Is(errors.Cause(err), fs.ErrNotExist) {
 			ctx.setError(
 				errors.Wrap(err, "getFileInfo in handling deconflict failed"),
 			)
@@ -347,10 +349,12 @@ func upload(ctx *ctx, src string, fi os.FileInfo, dst string, igs []ignPtn) {
 		}
 
 	case 4: // DeconflictLarger
-		if _, fis, err := getFileInfo(ctx, dst); err == nil && fi.Size() <= getFullSize(ctx, fis) {
-			fmt.Println("skip not larger file: " + src)
-			return
-		} else if err != nil && !errors.Is(errors.Cause(err), fs.ErrNotExist) {
+		if _, fis, err := getFileInfo(ctx, dst); err == nil {
+			if fi.Size() <= getFullSize(ctx, fis) {
+				fmt.Println("skip not larger file: " + src)
+				return
+			}
+		} else if !errors.Is(errors.Cause(err), fs.ErrNotExist) {
 			ctx.setError(
 				errors.Wrap(err, "getFileInfo in handling deconflict failed"),
 			)
